51d6742f007fa3a68e89b1ab1fa7db1465e891e1
Andreas <andreas@makeable.dk>
2018-10-16 15:19:46 +0200

    Revised glide

--- library/src/main/java/me/kaelaela/opengraphview/OpenGraphView.java
+++ library/src/main/java/me/kaelaela/opengraphview/OpenGraphView.java
@@ -22,6 +22,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
 
 import me.kaelaela.opengraphview.network.DefaultTaskManager;
 import me.kaelaela.opengraphview.network.model.OGData;
@@ -284,7 +285,11 @@ public class OpenGraphView extends RelativeLayout {
 
         Log.d("OpenGraphView", "loadImage from url: " + url);
 
-        Glide.with(getContext()).load(url).transition(DrawableTransitionOptions.withCrossFade()).into(mRoundableImageView);
+        Glide.with(getContext())
+                .load(url)
+                .apply(new RequestOptions().centerCrop())
+//                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(mRoundableImageView);
 
 //        Bitmap bitmap = mOGCache.getImage(url);
 //        if (bitmap == null) {
